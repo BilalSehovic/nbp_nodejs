c7f2df08f87f47b6dc15f8e577de4005a825dae0
izvanzemaljci <87817399+SanjaFJ@users.noreply.github.com>
2022-05-27 12:25:14 +0200

    Created linktypes

--- services/router.js
+++ services/router.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const users = require('../controllers/users.js');
 const badges = require('../controllers/badges.js');
 const comments = require('../controllers/comments.js');
+const linktypes = require('../controllers/linktypes.js');
 
 router.route('/users/:id?')
     .get(users.get);
@@ -12,5 +13,8 @@ router.route('/badges/:id?')
 
 router.route('/comments/:id?')
     .get(comments.get);
+
+router.route('/linktypes/:id?')
+    .get(linktypes.get);
 
 module.exports = router;
